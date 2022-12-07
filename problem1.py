82c39438cd45f3845d730f05efca7abf8779048d
digbeee <digby.pratt@lis.ac.uk>
2022-12-07 12:21:09 +0000

    Changed to PEP 8 style and changed "totals"

--- problem1.py
+++ problem1.py
@@ -1,6 +1,12 @@
 import numpy as np
-threes = np.sum(np.arange(0,1000,3))
-fives = np.sum(np.arange(0,1000,5))
-fifteens = np.sum(np.arange(0,1000,15))
-totals = (threes+fives)-fifteens
-print(totals)
+threes = np.sum(
+    np.arange(0, 1000, 3)
+    )
+fives = np.sum(
+    np.arange(0, 1000, 5)
+    )
+fifteens = np.sum(
+    np.arange(0, 1000, 15)
+    )
+total = (threes+fives) - fifteens
+print(total)
