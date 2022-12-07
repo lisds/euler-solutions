c36a6956d93702f4d4d01cc5c1a574d8f20c8bc6
digbeee <digby.pratt@lis.ac.uk>
2022-12-07 16:44:24 +0000

    First attempt at euler solutions problem 2

--- problem2.py
+++ problem2.py
@@ -1,20 +1,23 @@
 import numpy as np
 
 
-fibonacci = np.zeros(51)
-#creates an empty array for the sequence to go in to
+fibonacci = np.zeros(33)
+fibevens = np.zeros(33)
 
 pos = 2
-#pos is the current position which needs to equal the sum of the 2 positions before that. We need to assign the first two positions of the sequence to 1 and 2, then start at 3
-
 fibonacci[0] = 1
 fibonacci[1] = 2
-#first 2 numbers are set, now loop needs to start us at current pos, add the two positions prior, set current pos to the sum, then move the position up one
 
-
-for i in np.arange(2, 50):
+for i in np.arange(2, 32):
     magicnum = fibonacci[pos-1] + fibonacci[pos-2]
     fibonacci[i] = magicnum
     pos = pos + 1
-    if fibonacci[i] >= 4000000:
+    if fibonacci[i] >= 3000000:
         break
+for v in np.arange(33):
+    if fibonacci[v]%2 == 0:
+        fibevens[v] = fibonacci[v]
+    else:
+        pass
+answer = np.sum(fibevens)
+print(answer)
